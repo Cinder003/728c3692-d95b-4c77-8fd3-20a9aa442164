a09020dfad582dcc1730b61dca7b02bbe2920d79
Abraham James <abrahamjames2808003@gmail.com>
2025-11-14 11:28:03 +0530

    Add Front-end/src/App.tsx

---
+++ Front-end/src/App.tsx
@@ -0,0 +1,11 @@
+import TodoPage from './pages/TodoPage';
+
+function App() {
+  return (
+    <div className="min-h-screen w-full bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100 text-gray-800 font-sans">
+      <TodoPage />
+    </div>
+  );
+}
+
+export default App;
